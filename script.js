da2965a009d9c5eb4f0edfa3cb782d388e0f33d2
HONG1D <160582709+HONG1D@users.noreply.github.com>
2025-10-16 18:49:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 const options = {"actions": false};
 
 // Section 1
-vegaEmbed('#map', 'malaysia_tourism_map.vg.json', options);
-vegaEmbed('#scatter', 'malaysia_tourism_scatterplot.vg.json', options);
+vegaEmbed('#trend', 'malaysia_tourism_grouped_line_bar_chart.vg.json', options);
 
 // Section 2
 vegaEmbed('#heatmap', 'malaysia_tourism_heatmap.vg.json', options);
@@ -11,4 +10,6 @@ vegaEmbed('#grouped-bar', 'malaysia_tourism_grouped_diverging_bar.vg.json', opti
 vegaEmbed('#filter-bar', 'malaysia_tourism_filter_diverging_bar.vg.json', options);
 
 // Section 3
-vegaEmbed('#trend', 'malaysia_tourism_grouped_line_bar_chart.vg.json', options);
+vegaEmbed('#map1', 'malaysia_tourism_map.vg.json', options);
+vegaEmbed('#map2', 'malaysia_tourism_map_visitors.vg.json', options);
+vegaEmbed('#scatter', 'malaysia_tourism_scatterplot.vg.json', options);
